2244470934416aa1183b3bb8be116b82e14b9f32
a2018116509 <a2018116509@estudiantes.upsa.edu.bo>
2021-06-11 15:12:57 -0400

    Agregar archivos de proyecto.

---
+++ ApiProductor/Controllers/TranslateController.cs
@@ -0,0 +1,35 @@
+﻿namespace ApiProductor.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using Azure.Messaging.ServiceBus;
+    using System;
+    using System.Threading.Tasks;
+    using ApiProductor.Models;
+    using Newtonsoft.Json;
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TranslateController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<bool> EnviarAsync([FromBody] Traduccion traduccion) {
+            
+            string connectionString = "Endpoint=sb://queuetraductor.servicebus.windows.net/;SharedAccessKeyName=enviar;SharedAccessKey=2Awin9tlnHKtiP9GIKUGEg/IoM77nnHUnIiQOGcyLLY=;EntityPath=traducciones";
+            string queueName = "traducciones";
+            String mensaje = JsonConvert.SerializeObject(traduccion);
+            await using (ServiceBusClient client = new ServiceBusClient(connectionString))
+            {
+                // create a sender for the queue 
+                ServiceBusSender sender = client.CreateSender(queueName);
+
+                // create a message that we can send
+                ServiceBusMessage message = new ServiceBusMessage(mensaje);
+
+                // send the message
+                await sender.SendMessageAsync(message);
+                Console.WriteLine($"Sent a single message to the queue: {queueName}");
+            }
+
+            return true;
+        }
+    }
+}
