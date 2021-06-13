8c396318f04c644e34f578d10558a005ff96cdae
a2018116509 <a2018116509@estudiantes.upsa.edu.bo>
2021-06-13 15:56:05 -0400

    Function Completa

--- fncConsumidor/Function1.cs
+++ fncConsumidor/Function1.cs
@@ -1,16 +1,37 @@
-using System;
-using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Host;
-using Microsoft.Extensions.Logging;
-
 namespace fncConsumidor
 {
+    using fncConsumidor.Models;
+    using Microsoft.Azure.WebJobs;
+    using Microsoft.Extensions.Logging;
+    using Newtonsoft.Json;
+    using System;
+
     public static class Function1
     {
         [FunctionName("Function1")]
-        public static void Run([ServiceBusTrigger("traducciones", Connection = "MyConn")]string myQueueItem, ILogger log)
+        public static async System.Threading.Tasks.Task RunAsync(
+            [ServiceBusTrigger(
+                "traducciones", 
+                Connection = "MyConn"
+                )]string myQueueItem,
+            [CosmosDB(
+                    databaseName:"dbTranslate",
+                    collectionName:"Traducciones",
+                    ConnectionStringSetting = "strCosmos"
+                    )]IAsyncCollector<object> datos,
+            ILogger log)
         {
-            log.LogInformation($"C# ServiceBus queue trigger function processed message: {myQueueItem}");
+            try
+            {
+                log.LogInformation($"C# ServiceBus queue trigger function processed message: {myQueueItem}");
+                var data = JsonConvert.DeserializeObject<Traduccion>(myQueueItem);
+                await datos.AddAsync(data);
+            }
+            catch (Exception ex)
+            {
+                log.LogError($"No fue posible insertar datos: {ex.Message}");
+            }
+
         }
     }
 }
---
+++ fncConsumidor/Models/Traduccion.cs
@@ -0,0 +1,19 @@
+﻿namespace fncConsumidor.Models
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+    public class Traduccion
+    {
+        [Key]
+        public string NameDevice { get; set; }
+        [DataType(DataType.DateTime)]
+        [Required]
+        public DateTime Date { get; set; }
+        [Required]
+        public string Translation { get; set; }
+        [Required]
+        public string FromLanguage { get; set; }
+        [Required]
+        public string ToLanguage { get; set; }
+    }
+}
